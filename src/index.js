cc237f72cf50bfc8688ba033795041c38af44362
le huu phi <lehuuphi0@gmail.com>
2022-12-27 09:05:24 +0700

    project tiktok

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+// import React from 'react';
+// import App from './App.js';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>,
+// );
+// ⛔️ Warning: You are importing createRoot from "react-dom"
+// which is not supported. You should instead
+// import it from "react-dom/client"
+import { StrictMode } from 'react';
+
+import { createRoot } from 'react-dom'; // 👈️ wrong import path
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <StrictMode>
+        <App />
+    </StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
